0dad0c0acfe75ad4c0962f751386f9fe8457ffad
hironobu-igawa <zin920314@gmail.com>
2016-05-24 01:58:47 +0900

    Add dogs data

--- scripts/dog.js
+++ scripts/dog.js
@@ -927,6 +927,1016 @@
         ]
       },
       {
+        id: 47,
+        name: 'レークランド・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'ブラック',
+          'レバー',
+          'ブルー',
+          'レッド',
+          'ウィートン',
+          'レッド・グリズル'
+        ],
+        characteristics: [
+          '別名：パタデール・テリア',
+          '眉間の被毛が特徴的',
+          '断尾'
+        ],
+        initialWorks: [
+          '小型哺乳類の猟',
+          '捕殺'
+        ]
+      },
+      {
+        id: 48,
+        name: 'ウェルッシュ・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'ブラック＆タン',
+          'ブラック',
+          'グリズル＆タン'
+        ],
+        characteristics: [
+          '断尾'
+        ],
+        initialWorks: [
+          'ネズミ猟'
+        ]
+      },
+      {
+        id: 49,
+        name: 'ケリー・ブルー・テリア',
+        group: '3G',
+        countries: [
+          'アイルランド'
+        ],
+        colors: [
+          'ブルー'
+        ],
+        characteristics: [
+          '別名：青い悪魔',
+          '断尾',
+          '青みを帯びた美しい被毛'
+        ],
+        initialWorks: [
+          'アナグマ・キツネ・カワウソ・ネズミ猟'
+        ]
+      },
+      {
+        id: 50,
+        name: 'スコティッシュ・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'ブラック',
+          'ブリンドル',
+          'ウィートン'
+        ],
+        characteristics: [
+          '愛称：スコッティ',
+        ],
+        initialWorks: [
+          '小型哺乳類動物の猟'
+        ]
+      },
+      {
+        id: 51,
+        name: 'シーリハム・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'ホワイト'
+        ],
+        characteristics: [
+          '頭部と耳に班が入る'
+        ],
+        initialWorks: [
+          'アナグマ・カワウソ猟'
+        ]
+      },
+      {
+        id: 52,
+        name: 'ジャック・ラッセル・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          '白地にブラックあるいはタンの班'
+        ],
+        characteristics: [
+          '短毛：スムース',
+          '長毛：ラフ',
+          '粗毛：ブロークン'
+        ],
+        initialWorks: [
+          'ネズミ捕り'
+        ]
+      },
+      {
+        id: 53,
+        name: 'ワイヤーヘアード・フォックス・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          '白地にブラック班',
+          '白地にブラック＆タン班',
+          '白地にタン班'
+        ],
+        characteristics: [
+          '断尾',
+          '短毛腫：スムースヘアード・フォックス・テリア＝テリア界の貴公子'
+        ],
+        initialWorks: [
+          'キツネの追い立て',
+          '小型哺乳類の捕殺',
+          'うさぎ猟'
+        ]
+      },
+      {
+        id: 54,
+        name: 'アメリカン・スタッフォードシャー・テリア',
+        group: '3G',
+        countries: [
+          'アメリカ'
+        ],
+        colors: [
+          'オールカラー'
+        ],
+        characteristics: [
+          '別名：アメリカン・ビット・ブル・テリア'
+        ],
+        initialWorks: [
+          '牛攻め',
+          '闘犬'
+        ]
+      },
+      {
+        id: 55,
+        name: 'スタッフォードシャー・ブル・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'レッド',
+          'フォーン',
+          'ブルー',
+          'ブラック',
+          'ホワイト',
+          'ブリンドル'
+        ],
+        characteristics: [
+          'ブルテリアの祖先'
+        ],
+        initialWorks: [
+          '闘犬',
+          'ネズミ捕り'
+        ]
+      },
+      {
+        id: 56,
+        name: 'ブル・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'レッド',
+          'フォーン',
+          '純白',
+          'ブラック',
+          'トライカラー',
+          'ブリンドル'
+        ],
+        characteristics: [
+          '顔にスタンプがある',
+          '体色にカラーの多いもの＝カラード・ブル・テリア',
+          '体高35.5以下＝ミニチュア・ブル・テリア'
+        ],
+        initialWorks: [
+          '闘犬',
+          'コンパニオン'
+        ]
+      },
+      {
+        id: 57,
+        name: 'スカイ・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'グレー',
+          'フォーン',
+          'クリーム',
+          'ブラック'
+        ],
+        characteristics: [
+          '体長は体高の2倍'
+        ],
+        initialWorks: [
+          '小型獣の猟'
+        ]
+      },
+      {
+        id: 58,
+        name: 'オーストラリアン・テリア',
+        group: '3G',
+        countries: [
+          'オーストラリア'
+        ],
+        colors: [
+          'ブルー',
+          'スチール・ブルー',
+          'ダーク・グレー・ブルー'
+        ],
+        characteristics: [
+          '断尾',
+          '粗い毛質'
+        ],
+        initialWorks: [
+          '農場のネズミ捕り',
+          '番犬'
+        ]
+      },
+      {
+        id: 59,
+        name: 'ダンディ・ディンモント・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'ペッパー',
+          'マスタード'
+        ],
+        characteristics: [
+          '頭頂毛がワタ帽子のよう'
+        ],
+        initialWorks: [
+          'アナグマ・ネズミ猟'
+        ]
+      },
+      {
+        id: 60,
+        name: 'ベドリントン・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'ブルー',
+          'レバー',
+          'サンディ'
+        ],
+        characteristics: [
+          '頭部から顔面にかけて卵型にトリミングされている'
+        ],
+        initialWorks: [
+          'アナグマ・ネズミ猟'
+        ]
+      },
+      {
+        id: 61,
+        name: 'アイリッシュ・ソフトコーテッド・ウィートン・テリア',
+        group: '3G',
+        countries: [
+          'アイルランド'
+        ],
+        colors: [
+          'ウィートン'
+        ],
+        characteristics: [
+          '断尾'
+        ],
+        initialWorks: [
+          'アナグマ・カワウソ猟',
+          '小型害獣の狩猟'
+        ]
+      },
+      {
+        id: 62,
+        name: '日本・テリア',
+        group: '3G',
+        countries: [
+          '日本'
+        ],
+        colors: [
+          '頭：白・黒・茶',
+          '胴：白地に黒か茶の班'
+        ],
+        characteristics: [
+          '短毛で寒さに弱い',
+          '活動的'
+        ],
+        initialWorks: [
+          'ネズミ捕り',
+          '回収犬'
+        ]
+      },
+      {
+        id: 63,
+        name: 'ヨークシャー・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'ダーク・スチール・ブルー'
+        ],
+        characteristics: [
+          '断尾',
+          '別名：動く宝石',
+          '愛称：ヨーキー'
+        ],
+        initialWorks: [
+          'なし'
+        ]
+      },
+      {
+        id: 64,
+        name: 'トイ・マンチェスター・テリア',
+        group: '3G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'ブラック＆タン'
+        ],
+        characteristics: [
+          'サムマーク'
+        ],
+        initialWorks: [
+          'なし'
+        ]
+      },
+      {
+        id: 65,
+        name: 'オーストラリアン・シルキー・テリア',
+        group: '3G',
+        countries: [
+          'オーストラリア'
+        ],
+        colors: [
+          'ブルー＆タン'
+        ],
+        characteristics: [
+          '断尾',
+          '絹糸状で光沢のある被毛'
+        ],
+        initialWorks: [
+          'なし'
+        ]
+      },
+      {
+        id: 66,
+        name: 'ダックスフンド',
+        group: '4G',
+        countries: [
+          'ドイツ'
+        ],
+        colors: [
+          'レッド',
+          'クリーム',
+          'ブラック＆タン',
+          'チョコレート＆タン',
+          'ダップル',
+          'ブリンドル'
+        ],
+        characteristics: [
+          '大型：スタンダード',
+          '中型：ミニチュア',
+          '小型：カニーンヘン',
+          '短毛：スムースヘアード',
+          '長毛：ロングヘアード',
+          '粗毛：ワイヤーヘアード'
+        ],
+        initialWorks: [
+          'アナグマ・ウサギ猟'
+        ]
+      },
+      {
+        id: 67,
+        name: 'ビーグル',
+        group: '6G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'ハウンドカラー'
+        ],
+        characteristics: [
+          '尾の先は白い'
+        ],
+        initialWorks: [
+          'ウサギ猟'
+        ]
+      },
+      {
+        id: 68,
+        name: 'バセットハウンド',
+        group: '6G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'ハウンドカラー'
+        ],
+        characteristics: [
+          '二毛色'
+        ],
+        initialWorks: [
+          'アカシカ猟'
+        ]
+      },
+      {
+        id: 69,
+        name: 'ローデシアン・リッジバック',
+        group: '6G',
+        countries: [
+          '南アフリカ'
+        ],
+        colors: [
+          '赤みがかかったウィートン'
+        ],
+        characteristics: [
+          '背中にリッジがある'
+        ],
+        initialWorks: [
+          'ライオンの追跡'
+        ]
+      },
+      {
+        id: 70,
+        name: 'ブラッドハウンド',
+        group: '6G',
+        countries: [
+          'ベルギー'
+        ],
+        colors: [
+          'ブラック＆タン',
+          'レバー＆タン',
+          'レッド'
+        ],
+        characteristics: [
+          '正式名称：セントヒューバート・ブラッド・ハウンド'
+        ],
+        initialWorks: [
+          'シカ・イノシシ猟用のセントハウンド'
+        ]
+      },
+      {
+        id: 71,
+        name: 'プチバセットグリフォン・バンデーン',
+        group: '6G',
+        countries: [
+          'フランス'
+        ],
+        colors: [
+          'ブラック＆ホワイト',
+          'ブラック＆タン',
+          'ホワイト＆オレンジ',
+          'トライカラー'
+        ],
+        characteristics: [
+          'やや長めのボディ',
+          '硬くて粗い被毛'
+        ],
+        initialWorks: [
+          '野ウサギ猟'
+        ]
+      },
+      {
+        id: 72,
+        name: 'ダルメシアン',
+        group: '6G',
+        countries: [
+          'クロアチアのダルメシアン地方'
+        ],
+        colors: [
+          'ブラック',
+          'レバー'
+        ],
+        characteristics: [
+          '短毛',
+          '全身に水玉模様',
+          '尾が細い',
+          '垂れ耳'
+        ],
+        initialWorks: [
+          '実猟',
+          '運搬犬'
+        ]
+      },
+      {
+        id: 73,
+        name: 'アフガン・ハウンド',
+        group: '10G',
+        countries: [
+          'アフガニスタン'
+        ],
+        colors: [
+          'オールカラー'
+        ],
+        characteristics: [
+          'ブラックマスク',
+          'ドミノ'
+        ],
+        initialWorks: [
+          'ガゼルやヒョウのサイトハウンド'
+        ]
+      },
+      {
+        id: 74,
+        name: 'グレー・ハウンド',
+        group: '10G',
+        countries: [
+          'エジプト'
+        ],
+        colors: [
+          'ブラック',
+          'ブルー',
+          'ブリンドル',
+          'フォーン',
+          'ファロー',
+          'レッド',
+          'ホワイト'
+        ],
+        characteristics: [
+          '世界一足が速い',
+          '大：体高60〜80cm'
+        ],
+        initialWorks: [
+          'シカ狩り用のサイトハウンド',
+          'レーシングドッグ'
+        ]
+      },
+      {
+        id: 75,
+        name: 'ウィペット',
+        group: '10G',
+        countries: [
+          'イギリス'
+        ],
+        colors: [
+          'オールカラー'
+        ],
+        characteristics: [
+          '中：体高40〜60cm'
+        ],
+        initialWorks: [
+          'サイトハウンド',
+          'レージングドッグ',
+          'コンパニオンドッグ'
+        ]
+      },
+      {
+        id: 76,
+        name: 'イタリアン・グレー・ハウンド',
+        group: '10G',
+        countries: [
+          'イタリア'
+        ],
+        colors: [
+          'ブラック',
+          'ブルー',
+          'フォーン',
+          'イエロー',
+          'レッド',
+          'グレー'
+        ],
+        characteristics: [
+          '全身短毛',
+          '小：体高40cm以下'
+        ],
+        initialWorks: [
+          'レーシングドッグ',
+          'コンパニオンドッグ'
+        ]
+      },
+      {
+        id: 77,
+        name: 'サルーキ',
+        group: '10G',
+        countries: [
+          'エジプト'
+        ],
+        colors: [
+          'オールカラー'
+        ],
+        characteristics: [
+          'グリズル',
+          '短毛：スムース・バラエティ'
+        ],
+        initialWorks: [
+          'サイトハウンド'
+        ]
+      },
+      {
+        id: 78,
+        name: 'ボルゾイ',
+        group: '10G',
+        countries: [
+          'ロシア'
+        ],
+        colors: [
+          'ホワイトが優勢の方が好まれる'
+        ],
+        characteristics: [
+          '別名：ロシアン・ウルフ・ハウンド'
+        ],
+        initialWorks: [
+          'サイトハウンド',
+          'レーシングドッグ'
+        ]
+      },
+      {
+        id: 79,
+        name: 'アイリッシュ・ウルフハウンド',
+        group: '10G',
+        countries: [
+          'アイルランド'
+        ],
+        colors: [
+          'ブラック',
+          'ブリンンドル',
+          'フォーン',
+          'レッド',
+          'ピュアホワイト',
+          'グレー'
+        ],
+        characteristics: [
+          '最も体高が高い'
+        ],
+        initialWorks: [
+          'オオカミやシカのサイトハウンド'
+        ]
+      },
+      {
+        id: 80,
+        name: '日本スピッツ',
+        group: '5G',
+        countries: [
+          '日本'
+        ],
+        colors: [
+          '純白'
+        ],
+        characteristics: [
+          '豊かな純白の被毛をもつ'
+        ],
+        initialWorks: [
+          'コンパニオン'
+        ]
+      },
+      {
+        id: 81,
+        name: '柴犬',
+        group: '5G',
+        countries: [
+          '日本'
+        ],
+        colors: [
+          '赤',
+          '黒褐色',
+          '胡麻',
+          '黒胡麻',
+          '赤胡麻'
+        ],
+        characteristics: [
+          '一回り小さいものをマメシバという',
+          '裏白'
+        ],
+        initialWorks: [
+          '小型獣の狩猟犬'
+        ]
+      },
+      {
+        id: 82,
+        name: '四国犬',
+        group: '5G',
+        countries: [
+          '日本'
+        ],
+        colors: [
+          '赤',
+          '黒褐色',
+          '胡麻'
+        ],
+        characteristics: [
+          'なし'
+        ],
+        initialWorks: [
+          '狩猟犬'
+        ]
+      },
+      {
+        id: 83,
+        name: '紀州犬',
+        group: '5G',
+        countries: [
+          '日本'
+        ],
+        colors: [
+          '赤',
+          '白',
+          '胡麻'
+        ],
+        characteristics: [
+          'なし'
+        ],
+        initialWorks: [
+          '山岳でのイノシシ猟'
+        ]
+      },
+      {
+        id:  84,
+        name: '甲斐犬',
+        group: '5G',
+        countries: [
+          '日本'
+        ],
+        colors: [
+          '虎',
+          '赤虎',
+          '黒虎'
+        ],
+        characteristics: [
+          'なし'
+        ],
+        initialWorks: [
+          '大型獣の狩猟犬'
+        ]
+      },
+      {
+        id:  85,
+        name: '北海道犬',
+        group: '5G',
+        countries: [
+          '日本'
+        ],
+        colors: [
+          '白',
+          '赤',
+          '黒',
+          '胡麻',
+          '虎',
+          '黒褐色'
+        ],
+        characteristics: [
+          '別名：アイヌ犬'
+        ],
+        initialWorks: [
+          '大型獣の狩猟犬'
+        ]
+      },
+      {
+        id:  86,
+        name: '秋田犬',
+        group: '5G',
+        countries: [
+          '日本'
+        ],
+        colors: [
+          '白',
+          '赤',
+          '胡麻',
+          '虎'
+        ],
+        characteristics: [
+          '裏白'
+        ],
+        initialWorks: [
+          '大型獣の狩猟犬',
+          '闘犬'
+        ]
+      },
+      {
+        id:  87,
+        name: 'キースホンド',
+        group: '5G',
+        countries: [
+          'オランダ'
+        ],
+        colors: [
+          'ウルフ・グレー'
+        ],
+        characteristics: [
+          '豊かな被毛',
+          '顔に眼鏡模様がある',
+          '別名：ジャーマン・ウルフ・スピッツ'
+        ],
+        initialWorks: [
+          'はしけの歩哨',
+        ]
+      },
+      {
+        id:  88,
+        name: 'メキシカン・ヘアレス・ドッグ',
+        group: '5G',
+        countries: [
+          'メキシコ'
+        ],
+        colors: [
+          'オールカラー'
+        ],
+        characteristics: [
+          '無毛＝ヘアレス',
+          '有毛＝コーテッド'
+        ],
+        initialWorks: [
+          '宗教儀式'
+        ]
+      },
+      {
+        id:  89,
+        name: 'チャウ・チャウ',
+        group: '5G',
+        countries: [
+          '中国'
+        ],
+        colors: [
+          'レッド',
+          'ブラック',
+          'ブルー',
+          'フォーン',
+          'クリーム',
+          'ホワイト'
+        ],
+        characteristics: [
+          '舌が紫',
+          '後ろ足を竹馬状に運んで歩く',
+          '短毛種：スムース・チャウ・チャウ'
+        ],
+        initialWorks: [
+          '警備犬',
+          '荷車引き',
+          '食用'
+        ]
+      },
+      {
+        id:  90,
+        name: 'ポメラニアン',
+        group: '5G',
+        countries: [
+          'ドイツのポメラニア地方'
+        ],
+        colors: [
+          'オレンジ',
+          'ブラック',
+          'ブラウン',
+          'レッド',
+          'クリーム',
+          'ホワイト',
+          'チョコレート',
+          'ブラック＆タン',
+          'ウルフ・セーブル',
+          'ブルー'
+        ],
+        characteristics: [
+          'なし'
+        ],
+        initialWorks: [
+          'なし'
+        ]
+      },
+      {
+        id:  91,
+        name: 'バセンジー',
+        group: '5G',
+        countries: [
+          'コンゴ'
+        ],
+        colors: [
+          'ピュアブラック＆ホワイト',
+          'レッド＆ホワイト',
+          'タン＆ホワイト',
+          'ブラック＆タン',
+          'ホワイト',
+          'ブリンドル',
+          'ブラック'
+        ],
+        characteristics: [
+          '別名：コンゴ・テリア',
+          '眉間にシワ',
+          'ワンと鳴かない',
+          '体臭がない',
+          '発情が年に1回'
+        ],
+        initialWorks: [
+          '猟犬',
+          'コンパニオンドッグ'
+        ]
+      },
+      {
+        id:  92,
+        name: 'ファラオ・ハウンド',
+        group: '5G',
+        countries: [
+          'マルタ共和國'
+        ],
+        colors: [
+          'タン'
+        ],
+        characteristics: [
+          '別名：ラビットドッグ'
+        ],
+        initialWorks: [
+          'ガゼル・ウサギ猟'
+        ]
+      },
+      {
+        id:  93,
+        name: 'ノルウェジアン・エルクハウンド',
+        group: '5G',
+        countries: [
+          'ノルウェー'
+        ],
+        colors: [
+          'グレー'
+        ],
+        characteristics: [
+          '被毛は厚く豊富'
+        ],
+        initialWorks: [
+          'ヘラジカ猟'
+        ]
+      },
+      {
+        id:  94,
+        name: 'シベリアン・ハスキー',
+        group: '5G',
+        countries: [
+          'アメリカ'
+        ],
+        colors: [
+          'オールカラー'
+        ],
+        characteristics: [
+          '尾が垂れている',
+          'ケネル犬：晴',
+          '血統書名：ガーナー・オブ・コホク・マブチ・JP'
+        ],
+        initialWorks: [
+          'そり犬'
+        ]
+      },
+      {
+        id:  95,
+        name: 'アラスカン・マラミュート',
+        group: '5G',
+        countries: [
+          'アメリカのアラスカ地方'
+        ],
+        colors: [
+          'ライトグレー',
+          'ブラック',
+          'セーブル',
+          'レッド',
+          'ホワイト'
+        ],
+        characteristics: [
+          '尾が巻いている'
+        ],
+        initialWorks: [
+          'そり犬',
+          '狩猟犬'
+        ]
+      },
+      {
+        id:  96,
+        name: 'サモエド',
+        group: '5G',
+        countries: [
+          'ロシア北部・及びシベリア地方'
+        ],
+        colors: [
+          'ピュアホワイト',
+          'クリーム'
+        ],
+        characteristics: [
+          'サモエド・スマイル'
+        ],
+        initialWorks: [
+          'そり犬',
+          'トナカイ猟'
+        ]
+      },
+      {
         id: 97,
         name: 'イングリッシュ・ポインター',
         group: '7G',
